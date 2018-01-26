d3053518c55a8439561d731c2b4cc0c25d4ed66a
john.chen <ban77323@hotmail.com>
2018-01-26 08:22:11 +0000

    change network to coco dataset SSD mobileNet

--- flask_imageNet/image_classify/classify_api.py
+++ flask_imageNet/image_classify/classify_api.py
@@ -12,12 +12,14 @@ class NodeLookup(object):
                  label_lookup_path=None,
                  uid_lookup_path=None):
         if not label_lookup_path:
-            label_lookup_path = 'models/imagenet_2012_challenge_label_map_proto.pbtxt'
+            #label_lookup_path = 'models/imagenet/imagenet_2012_challenge_label_map_proto.pbtxt'
+            label_lookup_path = 'models/coco/mscoco_label_map.pbtxt'
         if not uid_lookup_path:
-            uid_lookup_path = 'models/imagenet_synset_to_human_label_map.txt'
+            uid_lookup_path = 'models/imagenet/imagenet_synset_to_human_label_map.txt'
         self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
 
     def load(self, label_lookup_path, uid_lookup_path):
+        """
         if not tf.gfile.Exists(uid_lookup_path):
             tf.logging.fatal('File does not exist %s', uid_lookup_path)
         if not tf.gfile.Exists(label_lookup_path):
@@ -30,30 +32,44 @@ class NodeLookup(object):
             uid = parsed_items[0]
             human_string = parsed_items[2]
             uid_to_human[uid] = human_string
+        """
         node_id_to_uid = {}
         proto_as_ascii = tf.gfile.GFile(label_lookup_path).readlines()
         for line in proto_as_ascii:
-            if line.startswith('  target_class:'):
+            if line.startswith('  id:'):
                 target_class = int(line.split(': ')[1])
-            if line.startswith('  target_class_string:'):
+            if line.startswith('  display_name:'):
                 target_class_string = line.split(': ')[1]
                 node_id_to_uid[target_class] = target_class_string[1:-2]
+        print('john123 node_id_to_uid: %s' % node_id_to_uid)
+        print('john type %s' % type(node_id_to_uid))
+        print('john test dict %s' % node_id_to_uid[2])
+        return node_id_to_uid
+
+        """
         node_id_to_name = {}
         for key, val in node_id_to_uid.items():
             if val not in uid_to_human:
                 tf.logging.fatal('Failed to locate: %s', val)
             name = uid_to_human[val]
             node_id_to_name[key] = name
-
+            # node_id_to_name ={id1:name1, id2:name2, ...}
         return node_id_to_name
+        """
 
     def id_to_string(self, node_id):
-        if node_id not in self.node_lookup:
+        print('john node_id type %s' % type(node_id))
+        print('john node_id %s' % node_id)
+        print('john node_lookup %s' % self.node_lookup)
+
+        if int(node_id) not in self.node_lookup:
+            print('123456')
+            print('node_lookup %s' % self.node_lookup)
             return ''
         return self.node_lookup[node_id]
 
 
-with tf.gfile.FastGFile('models/classify_image_graph_def.pb', 'rb') as f:
+with tf.gfile.FastGFile('models/coco/frozen_inference_graph.pb', 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
@@ -69,12 +85,18 @@ def classify(image_data):
                            {'DecodeJpeg/contents:0': image_data})
     predictions = np.squeeze(predictions)
     top_k = predictions.argsort()[-10:][::-1]
+    print('classify result top_k: %s' % top_k)
+    print('classify predictions len: %s' % len(predictions))
+
     results = []
     for node_id in top_k:
+        
         human_string = node_lookup.id_to_string(node_id)
         score = predictions[node_id]
+        print('john human_string %s' % human_string)
         # print('%s (score = %.5f)' % (human_string, score))
         results.append({'label': human_string, 'score': '{:.2}'.format(score)})
+    print('classify return results: %s' % results)
     return results
 
 
